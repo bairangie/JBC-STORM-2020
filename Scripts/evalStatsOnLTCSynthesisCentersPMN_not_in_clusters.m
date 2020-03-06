e466db7f2b4a16d45faed5d93362bac21ebfa4b7
Nicholas Bauer <nicholasbauer@outlook.com>
2020-03-06 13:28:46 -0500

    Added scripts, README

---
+++ Scripts/evalStatsOnLTCSynthesisCentersPMN_not_in_clusters.m
@@ -0,0 +1,162 @@
+rootPath = "C:\Users\nicho\Desktop\For Angie\2019 PMN REVISIONS ClusDoC\";
+
+fprintf("c5a 5LO CPLA2\n");
+
+getPointsInCluster(rootPath + "m1 c5a\results c5a 5lo561 cpla2647\", 5, 5);
+getPointsInCluster(rootPath + "m1 c5a\results c5a 5lo561 cpla2648\", 5, 5);
+getPointsInCluster(rootPath + "m2 c5a\results m2 c5a 5lo561 cpla2 648\", 5, 5);
+getPointsInCluster(rootPath + "m2 c5a\results m2 c5a 5lo561 cpla2 649\", 5, 5);
+getPointsInCluster(rootPath + "m2 c5a\results m2 c5a 5lo561 cpla2 650\", 5, 5);
+
+
+fprintf("c5a 5LO FLAP\n");
+
+getPointsInCluster(rootPath + "m1 c5a\results c5a 5lo561 flap647 2\", 5, 10);
+getPointsInCluster(rootPath + "m2 c5a\results m2 c5a 5lo561 flap647\", 5, 10);
+getPointsInCluster(rootPath + "m2 c5a\results m2 c5a 5lo561 flap648\", 5, 10);
+getPointsInCluster(rootPath + "m2 c5a\results m2 c5a 5lo561 flap649\", 5, 10);
+getPointsInCluster(rootPath + "m3 c5a\c5a 5lo561 flap647\", 5, 10);
+getPointsInCluster(rootPath + "m3 c5a\c5a 5lo561 flap648\", 5, 10);
+
+
+fprintf("c5a cPLA2 FLAP");
+
+getPointsInCluster(rootPath + "m1 c5a\results c5a flap647   cpla2561\", 5, 10);
+getPointsInCluster(rootPath + "m1 c5a\results c5a flap647 cpla2561\", 5, 10);
+getPointsInCluster(rootPath + "m2 c5a\results m2 c5a cpla2561 flap647\", 5, 10);
+
+
+fprintf("GM c5a 5LO cPLA2\n");
+
+getPointsInCluster(rootPath + "m1 gmc5a\results gmc5a 5lo561 cpla647\", 5, 5);
+getPointsInCluster(rootPath + "m1 gmc5a\results gmc5a 5lo561 cpla651\", 5, 5);
+getPointsInCluster(rootPath + "m1 gmc5a\results gmc5a 5lo561 cpla652\", 5, 5);
+getPointsInCluster(rootPath + "m2 gmc5a\results m2 gmc5a 5lo561 cpla2 647\", 5, 5);
+getPointsInCluster(rootPath + "m2 gmc5a\results m2 gmc5a 5lo561 cpla2 648\", 5, 5);
+getPointsInCluster(rootPath + "m2 gmc5a\results m2 gmc5a 5lo561 cpla2 650\", 5, 5);
+getPointsInCluster(rootPath + "m3 gmc5a\m3 gmc5a 5lo561 cpla2647 results\", 5, 5);
+getPointsInCluster(rootPath + "m3 gmc5a\m3 gmc5a 5lo561 cpla2650 results\", 5, 5);
+getPointsInCluster(rootPath + "m3 gmc5a\m3 gmc5a 5lo561 cpla2651 results\", 5, 5);
+
+
+fprintf("GM c5a 5LO FLAP\n");
+
+getPointsInCluster(rootPath + "m1 gmc5a\results gmc5a 5lo561 flap647\", 5, 10);
+getPointsInCluster(rootPath + "m1 gmc5a\results gmc5a 5lo647 flap647\", 5, 10);
+%getPointsInCluster(rootPath + "m2 gmc5a\results m2 gmc5a 5lo561 flap647\", 5, 10);
+getPointsInCluster(rootPath + "m2 gmc5a\results m2 gmc5a 5lo561 flap648\", 5, 10);
+getPointsInCluster(rootPath + "m2 gmc5a\results m2 gmc5a 5lo561 flap649\", 5, 10);
+getPointsInCluster(rootPath + "m3 gmc5a\m3 gmc5a 5lo561 flap647 results\", 5, 10);
+getPointsInCluster(rootPath + "m3 gmc5a\m3 gmc5a 5lo561 flap649 results\", 5, 10);
+
+
+fprintf("GM c5a cPLA2 FLAP\n");
+getPointsInCluster(rootPath + "m2 gmc5a\results m2 gmc5a cpla2561 flap648\", 5, 10);
+getPointsInCluster(rootPath + "m2 gmc5a\results m2 gmc5a cpla2561 flap649\", 5, 10);
+getPointsInCluster(rootPath + "m3 gmc5a\m3 gmc5a cpla2561 flap647 results\", 5, 10);
+getPointsInCluster(rootPath + "m3 gmc5a\m3 gmc5a cpla2561 flap648 results\", 5, 10);
+getPointsInCluster(rootPath + "m3 gmc5a\m3 gmc5a cpla2561 flap649 results\", 5, 10);
+getPointsInCluster(rootPath + "m3 gmc5a\m3 gmc5a cpla2561 flap650 results\", 5, 10);
+getPointsInCluster(rootPath + "m3 gmc5a\m3 gmc5a cpla2561 flap651 results\", 5, 10);
+getPointsInCluster(rootPath + "m3 gmc5a\m3 gmc5a cpla2561 flap651a results\", 5, 10);
+
+
+fprintf("unstim 5LO cPLA2\n");
+
+getPointsInCluster(rootPath + "m1 unstim\results unstim 5lo561 cpla2647\", 5, 5);
+getPointsInCluster(rootPath + "m1 unstim\results unstim 5lo561 cpla2648\", 5, 5);
+getPointsInCluster(rootPath + "m1 unstim\results unstim 5lo561 cpla2649\", 5, 5);
+getPointsInCluster(rootPath + "m2 unstim\results m2 unstim 5lo641 cpla2647\", 5, 5);
+getPointsInCluster(rootPath + "m3 unstim\unstim 5lo651 cpla2647\", 5, 5);
+getPointsInCluster(rootPath + "m4 unstim\m4 unstim 5lp561 cpla2647\", 5, 5);
+
+
+fprintf("unstim 5LO FLAP\n");
+
+getPointsInCluster(rootPath + "m1 unstim\results unstim 5lo561 flap647\", 5, 10);
+getPointsInCluster(rootPath + "m2 unstim\results m2 unstim 5lo561 flap647\", 5, 10);
+getPointsInCluster(rootPath + "m2 unstim\results m2 unstim 5lo561 flap648\", 5, 10);
+getPointsInCluster(rootPath + "m2 unstim\results m2 unstim 5lo641 flap647\", 5, 10);
+getPointsInCluster(rootPath + "m3 unstim\unstim 5lo651 flap648\", 5, 10);
+
+
+fprintf("unstim cPLA2 FLAP\n");
+
+getPointsInCluster(rootPath + "m3 unstim\unstim cpla2651 flap647\", 5, 10);
+getPointsInCluster(rootPath + "m4 unstim\m4 unstim cpla2561 flap647\", 5, 10);
+getPointsInCluster(rootPath + "m4 unstim\m4 unstim cpla2561 flap648\", 5, 10);
+
+
+
+fprintf("area, density Ch1, density Ch2, number Ch1, number Ch2, clusters, fraction interacting in clusters Ch1, fraction interacting in clusters Ch2\n");
+    
+function getPointsInCluster(rootPath, clusterInThresholdCh1, clusterInThresholdCh2)
+    clusterFileName = "Clus-DoC Results\DBSCAN Clus-DoC Results.mat";
+    pointsFileName = "Clus-DoC Results\Data_for_Cluster_Analysis.mat";
+
+    load(rootPath + clusterFileName);
+    load(rootPath + pointsFileName);
+    
+    numROI = size(ClusterSmoothTableCh1, 1);
+    
+    synthesisCenters = cell([numROI 1]);
+    
+    for i = 1:numROI
+        roiCh1 = ClusterSmoothTableCh1{i,1};
+        roiCh2 = ClusterSmoothTableCh2{i,1};
+        
+        roiPointsData = Data_DoC{i,1};
+        
+        numClustersCh1 = size(roiCh1, 1);
+        numClustersCh2 = size(roiCh2, 1);
+        
+        synthesisCentersCh1 = cell(0);
+        synthesisCentersCh2 = cell(0);
+        synthesisCenterCh1Count = 0;
+        synthesisCenterCh2Count = 0;
+        
+        pointsCh1 = [roiPointsData.X(roiPointsData.Ch == 1) roiPointsData.Y(roiPointsData.Ch == 1)];
+        pointsDoCCh1 = roiPointsData.DoC(roiPointsData.Ch == 1);
+        inCluster1 = false([numel(pointsDoCCh1) 1]);
+        pointsCh2 = [roiPointsData.X(roiPointsData.Ch == 2) roiPointsData.Y(roiPointsData.Ch == 2)];
+        pointsDoCCh2 = roiPointsData.DoC(roiPointsData.Ch == 2);
+        inCluster2 = false([numel(pointsDoCCh2) 1]);
+                
+        for j = 1:numClustersCh1
+            cluster = roiCh1{j,1};
+
+            if cluster.Nb < clusterInThresholdCh1
+                continue;
+            end
+                        
+            inCluster1(inpoly(pointsCh1, cluster.Contour)) = true;
+            inCluster2(inpoly(pointsCh2, cluster.Contour)) = true;
+        end
+        
+        for j = 1:numClustersCh2
+            cluster = roiCh2{j,1};
+            
+            if cluster.Nb < clusterInThresholdCh2
+                continue;
+            end
+                     
+            inCluster1(inpoly(pointsCh1, cluster.Contour)) = true;
+            inCluster2(inpoly(pointsCh2, cluster.Contour)) = true;
+        end
+        
+        countInteractingInCluster1 = nnz(inCluster1 & pointsDoCCh1 > 0.4);
+        countInteractingInCluster2 = nnz(inCluster2 & pointsDoCCh2 > 0.4);
+                
+        fractionInteractingInCluster1 = countInteractingInCluster1 / nnz(inCluster1);
+        fractionInteractingInCluster2 = countInteractingInCluster2 / nnz(inCluster2);
+        
+        countInteractingNotInCluster1 = nnz(~inCluster1 & pointsDoCCh1 > 0.4);
+        countInteractingNotInCluster2 = nnz(~inCluster2 & pointsDoCCh2 > 0.4);
+         
+        fractionInteractingNotInCluster1 = countInteractingNotInCluster1 / numel(inCluster1);
+        fractionInteractingNotInCluster2 = countInteractingNotInCluster2 / numel(inCluster2);
+        
+        fprintf("%f, %f, %f, %f\n", ...
+            fractionInteractingInCluster1, fractionInteractingInCluster2, fractionInteractingNotInCluster1, fractionInteractingNotInCluster2);
+    end
+end
